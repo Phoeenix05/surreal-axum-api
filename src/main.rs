182858e906d8fef98a5a19e57dab566ed433317b
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-05-12 21:19:59 +0300

    added `docs/index.html`

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,53 @@
-use axum::{routing::get, Router, extract::Query};
-use utoipa::OpenApi;
+use axum::{
+    extract::Path,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use colored::Colorize;
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
+use utoipa::{OpenApi, ToSchema};
+
+// use surrealdb::engine::remote::ws::Ws;
+// use surrealdb::opt::auth::Root;
+// use surrealdb::sql::Thing;
+// use surrealdb::Surreal;
 
 #[derive(OpenApi)]
-#[openapi(paths(get_user))]
+#[openapi(paths(get_user, create_user), components(schemas(User)))]
 struct ApiDoc;
 
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+struct User {}
+
 #[tokio::main]
-async fn main() {
-    println!("{}", ApiDoc::openapi().to_pretty_json().unwrap());
-    
+async fn main() -> surrealdb::Result<()> {
+    let apidoc = ApiDoc::openapi().to_pretty_json().unwrap();
+    match std::fs::write("openapi.json", apidoc) {
+        Ok(_) => println!("{}", "Saved ApiDoc".green().bold()),
+        Err(err) => println!("{}: {}", "Failed to save ApiDoc".red(), err),
+    }
+    // println!("{}", ApiDoc::openapi().to_pretty_json().unwrap());
+
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
         .with_max_level(tracing::Level::TRACE)
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-    let app = Router::new().route("/user/:name", get(get_user));
+    // let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
+    // db.signin(Root {
+    //     username: "root",
+    //     password: "root",
+    // })
+    // .await?;
+
+    // db.use_ns("test").use_db("test").await?;
+
+    let app = Router::new()
+        .route("/user/:name", get(get_user))
+        .route("/create/user", post(create_user));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
     tracing::debug!("listening on {}", addr);
@@ -24,19 +55,34 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
 
 #[utoipa::path(
     get,
     path = "/user/:name",
     responses(
-        (status = 200, description = "User found succesfully"),
+        (status = 200, description = "User found successfully"),
         (status = NOT_FOUND, description = "User not found")
     ),
     params(
         ("name" = String, description = "User's name")
     )
 )]
-async fn get_user(name: Option<Query<String>>) -> &'static str {
-    "Hello, World!"
+async fn get_user(Path(name): Path<String>) -> impl IntoResponse {
+    Json(name.to_string())
+}
+
+#[utoipa::path(
+    post,
+    path = "/create/user",
+    responses(
+        (status = 200, description = "User created successfully"),
+        (status = 400, description = "Creating new user failed")
+    ),
+    request_body = User
+)]
+async fn create_user(Json(_data): Json<User>) -> impl IntoResponse {
+    Json("ok")
 }
