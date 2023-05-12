7e755cc95759ad60d3ef09d6c9d3e31f2c53927a
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-05-12 17:29:17 +0300

    added utoipa

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,42 @@
-// use std::net::SocketAddr;
+use axum::{routing::get, Router, extract::Query};
+use utoipa::OpenApi;
+use std::net::SocketAddr;
 
-// use axum::{
-//     http::StatusCode,
-//     response::IntoResponse,
-//     routing::{get, post},
-//     Json, Router,
-// };
+#[derive(OpenApi)]
+#[openapi(paths(get_user))]
+struct ApiDoc;
 
 #[tokio::main]
 async fn main() {
-    // tracing_subscriber::fmt::init();
+    println!("{}", ApiDoc::openapi().to_pretty_json().unwrap());
+    
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
 
-    // let app = Router::new().route("/", get(root));
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-    // let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
-    // axum::serve
+    let app = Router::new().route("/user/:name", get(get_user));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-// async fn root() -> &'static str {
-//     "Hello, Axum!"
-// }
+#[utoipa::path(
+    get,
+    path = "/user/:name",
+    responses(
+        (status = 200, description = "User found succesfully"),
+        (status = NOT_FOUND, description = "User not found")
+    ),
+    params(
+        ("name" = String, description = "User's name")
+    )
+)]
+async fn get_user(name: Option<Query<String>>) -> &'static str {
+    "Hello, World!"
+}
