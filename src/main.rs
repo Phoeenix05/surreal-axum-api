ca27fcd19f1cd2292c34fae9ee9030c986393e03
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-05-12 16:52:22 +0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+// use std::net::SocketAddr;
+
+// use axum::{
+//     http::StatusCode,
+//     response::IntoResponse,
+//     routing::{get, post},
+//     Json, Router,
+// };
+
+#[tokio::main]
+async fn main() {
+    // tracing_subscriber::fmt::init();
+
+    // let app = Router::new().route("/", get(root));
+
+    // let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    // axum::serve
+}
+
+// async fn root() -> &'static str {
+//     "Hello, Axum!"
+// }
